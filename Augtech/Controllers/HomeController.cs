40495a79f420424fd633eeb99f77595aa2f2587b
Joe Gotthelf <jgotthelf@gmail.com>
2015-12-14 20:40:52 -0500

    Initial Check-in

---
+++ Augtech/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Augtech.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = string.Empty;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "August Technologies, Inc.";
+
+            return View();
+        }
+    }
+}
